9a8264829d3853a174f0297e10c0a761a6a62364
KirillKulakow <kirillkulakow31@gmail.com>
2020-08-26 17:55:49 +0300

    add error handler

--- api/DBData/DataControler.js
+++ api/DBData/DataControler.js
@@ -22,6 +22,12 @@ async function getTransactionForStatistic(req, res) {
       let { type, month, year } = req.query;
       month = Number(month);
       year = Number(year);
+      if(Number.isNaN(month) || Number.isNaN(year)) {
+        res.status(400).send({ message: "Bad request: month and year must be a number"})
+      }
+      if(month > 12) {
+        res.status(400).send({ message: "Bad request: uncorrect month"})
+      }
       const dateNow = new Date();
       if (month === undefined){
           month = dateNow.getMonth() + 1;
